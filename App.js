615b1b8f12ba2a8d4bcedb67628ed73b2d75eab8
Stive627 <fossistive627@gmail.com>
2025-05-23 01:02:50 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import './global.css'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className='text-blue-700 font-bold'>My friend </Text>
       <StatusBar style="auto" />
     </View>
   );
